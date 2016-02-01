466c1ab47345061a772851d9093cc911676026bf
Kevin Mooney <kmooney517@gmail.com>
2016-02-01 14:11:58 -0500

    changed to a different database on mongolabs

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
 var express    = require('express');
 var app        = express();
 var mongojs    = require('mongojs');
-var db         = mongojs('mongodb://kmooney517:Allymooney2!@ds051625.mongolab.com:51625/refresh', ['contactList']);
+var db         = mongojs('mongodb://chairleg:maggie@ds055535.mongolab.com:55535/mean_template', ['items']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/contactList', function(req, res) {
+app.get('/items', function(req, res) {
 
-  db.contactList.find(function(err, docs) {
+  db.items.find(function(err, docs) {
 
     res.json(docs);
   });
 
 });
 
-app.post('/contactList', function(req, res) {
-  db.contactList.insert(req.body, function(err, doc) {
+app.post('/items', function(req, res) {
+  db.items.insert(req.body, function(err, doc) {
     res.json(doc);
   });
 });
 
-app.delete('/contactList/:id', function(req, res) {
+app.delete('/items/:id', function(req, res) {
   var id = req.params.id;
 
   db.contactList.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
@@ -30,18 +30,18 @@ app.delete('/contactList/:id', function(req, res) {
   });
 });
 
-app.get('/contactList/:id', function(req, res) {
+app.get('/items/:id', function(req, res) {
   var id = req.params.id;
 
-  db.contactList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+  db.items.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
     res.json(doc);
   });
 });
 
-app.put('/contactList/:id', function(req, res) {
+app.put('/items/:id', function(req, res) {
   var id = req.params.id;
 
-  db.contactList.findAndModify({
+  db.items.findAndModify({
     query: {
       _id: mongojs.ObjectId(id)
     },
